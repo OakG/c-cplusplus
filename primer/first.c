eb39c4475ca5d7b8c5c06cf4517c011818948a8d
OakG <oak.garcia@gmail.com>
2018-07-30 21:47:07 -0300

    Baby steps

---
+++ primer/first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    int num;
+    num = 1;
+
+    printf("I am a simpleton. ");
+    printf("I am computer.\n");
+    printf("My fav number is %d because I say so.\n",num);
+
+    return 0;
+}
